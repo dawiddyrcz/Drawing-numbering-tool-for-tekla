4cde49ad46c83411ccab041fab24a2729e0fca78
Dawid Dyrcz <dyrdaw@gmail.com>
2018-12-18 22:07:40 +0100

    First issue of plugin

---
+++ VisualStudio2017/DrawingNumberingPlugin/DrawingNumberingPlugin_Form.cs
@@ -0,0 +1,169 @@
+﻿/* Copyright 2018 Dawid Dyrcz 
+ * See License.txt file 
+ */
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Tekla.Structures.Dialog;
+
+namespace DrawingNumberingPlugin
+{
+    public partial class DrawingNumberingPluginForm : PluginFormBase
+    {
+        public DrawingNumberingPluginForm()
+        {
+            InitializeComponent();
+            GenerateExampleNumbers();
+            EnableDisableControls();
+            FillTitleCombobox();
+        }
+
+        private void FillTitleCombobox()
+        {
+            this.title_comboBox.Items.Clear();
+            //DO NOT CHANGE THIS CODE
+            this.title_comboBox.Items.Add("NAME");
+            this.title_comboBox.Items.Add("TITLE1");
+            this.title_comboBox.Items.Add("TITLE2");
+            this.title_comboBox.Items.Add("TITLE3");
+
+            //This try is stupid but i dont know how to test it
+            try
+            {
+                var udaLabels = new UDAHandler().GetAllUDALabels();
+                this.title_comboBox.Items.AddRange(udaLabels.ToArray());
+            }
+            catch (Exception) { }
+        }
+
+        private void okApplyModifyGetOnOffCancel1_OnOffClicked(object sender, EventArgs e)
+        {
+            this.ToggleSelection();
+        }
+
+        private void GenerateExampleNumbers()
+        {
+            if (int.TryParse(this.startNumber_numericUpDown.Text, out int startNumber))
+            {
+                if (startNumber >= 0)
+                {
+                    string exampleText = "";
+
+                    for (int i = startNumber; i < startNumber + 7; i++)
+                    {
+                        string newLine = GetCurrentNumberWithPrefixAndPostFix(this.prefix_textBox.Text, this.postfix_textBox.Text, i);
+                        exampleText = exampleText + newLine + "\n";
+                    }
+
+                    this.example_label.Text = exampleText; 
+                }
+            }
+        }
+
+        private string GetCurrentNumberWithPrefixAndPostFix(string prefix, string postfix, int currentNumber)
+        {
+            if (onlyPrefix_checkBox.Checked) return prefix;
+            else return prefix + GetCurrentNumber(currentNumber) + postfix;
+        }
+
+        private string GetCurrentNumber(int currentNumber)
+        {
+            if (int.TryParse(this.digits_numericUpDown.Text, out int digits))
+            {
+                if (digits > 0)
+                {
+                    //Check number of digits in currentnumber and corrects var digits
+                    int.TryParse(Math.Max(digits, Math.Floor(Math.Log10(currentNumber) + 1)).ToString(), out digits);
+                    
+                    string formatString = "";
+
+                    for (int j = 0; j < digits; j++)
+                    {
+                        formatString = formatString + "0";
+                    }
+
+                    return currentNumber.ToString(formatString);
+                }
+                else
+                    return "";
+            }
+            else
+                return "";
+        }
+
+        private void prefix_textBox_TextChanged(object sender, EventArgs e)
+        {
+            GenerateExampleNumbers();
+        }
+
+        private void startNumber_numericUpDown_ValueChanged(object sender, EventArgs e)
+        {
+            GenerateExampleNumbers();
+        }
+
+        private void digits_numericUpDown_ValueChanged(object sender, EventArgs e)
+        {
+            GenerateExampleNumbers();
+        }
+
+        private void postfix_textBox_TextChanged(object sender, EventArgs e)
+        {
+            GenerateExampleNumbers();
+        }
+
+        private void createApplyCancel1_ApplyClicked(object sender, EventArgs e)
+        {
+            GenerateExampleNumbers();
+            this.Apply();
+        }
+
+        private void createApplyCancel1_CancelClicked(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void createApplyCancel1_CreateClicked(object sender, EventArgs e)
+        {
+            this.Apply();
+            this.Create();
+        }
+
+        private void startNumber_numericUpDown_MouseUp(object sender, System.Windows.Forms.MouseEventArgs e)
+        {
+            GenerateExampleNumbers();
+        }
+
+        private void digits_numericUpDown_MouseUp(object sender, System.Windows.Forms.MouseEventArgs e)
+        {
+            GenerateExampleNumbers();
+        }
+
+        private void onlyPrefix_checkBox_MouseUp(object sender, System.Windows.Forms.MouseEventArgs e)
+        {
+            GenerateExampleNumbers();
+            EnableDisableControls();
+        }
+
+        private void EnableDisableControls()
+        {
+            if (!onlyPrefix_checkBox.Checked)
+            {
+                startNumber_numericUpDown.Enabled = true;
+                postfix_textBox.Enabled = true;
+                digits_numericUpDown.Enabled = true;
+            }
+            else
+            {
+                startNumber_numericUpDown.Enabled = false;
+                postfix_textBox.Enabled = false;
+                digits_numericUpDown.Enabled = false;
+            }
+        }
+
+        private void saveLoad1_AttributesLoaded(object sender, EventArgs e)
+        {
+            GenerateExampleNumbers();
+            EnableDisableControls();
+        }
+    }
+}
---
+++ VisualStudio2017/DrawingNumberingPlugin/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("DrawingNumberingPlugin")]
+[assembly: AssemblyDescription("Tekla Structures plugin. Drawing numbering tool")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Dawid Dyrcz")]
+[assembly: AssemblyProduct("DrawingNumberingPlugin")]
+[assembly: AssemblyCopyright("Copyright © Dawid Dyrcz 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("97196bc5-5c5b-4476-a474-271f5c143f78")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.1.0")]
+[assembly: AssemblyFileVersion("1.0.1.0")]
