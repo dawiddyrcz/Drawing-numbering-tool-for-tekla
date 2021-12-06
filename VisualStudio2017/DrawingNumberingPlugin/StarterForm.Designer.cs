57355217844593929468f25f76c4bbf6deafe719
Dawid Dyrcz <dyrdaw@gmail.com>
2021-12-06 20:08:15 +0100

    Added plugin which starts application

---
+++ VisualStudio2017/DrawingNumberingPlugin/StarterForm.Designer.cs
@@ -0,0 +1,68 @@
+﻿namespace DrawingNumberingPlugin
+{
+    partial class StarterForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(StarterForm));
+            this.start_button = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // start_button
+            // 
+            this.start_button.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.start_button.Location = new System.Drawing.Point(0, 0);
+            this.start_button.Name = "start_button";
+            this.start_button.Size = new System.Drawing.Size(212, 47);
+            this.start_button.TabIndex = 0;
+            this.start_button.Text = "Run Drawing Numbering Tool";
+            this.start_button.UseVisualStyleBackColor = true;
+            this.start_button.Click += new System.EventHandler(this.Start_button_Click);
+            // 
+            // StarterForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(212, 47);
+            this.Controls.Add(this.start_button);
+            this.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "StarterForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "StarterForm";
+            this.TopMost = true;
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button start_button;
+    }
+}
---
+++ VisualStudio2017/DrawingNumberingPlugin/StarterForm.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.IO;
+using Tekla.Structures.Dialog;
+
+namespace DrawingNumberingPlugin
+{
+    public partial class StarterForm : PluginFormBase
+    {
+        private readonly string _appName = "DrawingNumberingApp.exe";
+        private readonly string _tsepDirName = "DrawingNumberingPlugin";
+        private readonly string _appDirectory = System.IO.Path.Combine("applications", "Tekla", "DrawingNumberingApp");
+        public StarterForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Start_button_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                try
+                {
+                    CloseAllProcesses();
+                }
+                catch { }
+
+                var appFullPath = GetApplicationExePath();
+                System.Diagnostics.Process.Start(appFullPath);
+
+                Tekla.Structures.Model.Operations.Operation.DisplayPrompt(
+                    DateTime.Now.ToString("HH:mm:ss.fff") + " Trying to start application: " + appFullPath);
+            }
+            catch (Exception ex)
+            {
+                System.Windows.Forms.MessageBox.Show(ex.ToString());
+            }
+
+            Invoke(new Action(() =>
+            {
+                try
+                {
+                    System.Threading.Thread.Sleep(50);
+                    this.Close();
+                }
+                catch { }
+            }
+            ));
+        }
+
+        private string GetApplicationExePath()
+        {
+            string appFileFullPath = "";
+
+            string XS_Variable = System.Environment.GetEnvironmentVariable("XSDATADIR");
+            string extensionDirectory = Path.Combine(XS_Variable, "Environments", "common", "extensions");
+
+            if (!string.IsNullOrWhiteSpace(XS_Variable))
+                appFileFullPath = Path.Combine(extensionDirectory, _tsepDirName, _appName);
+
+            if (File.Exists(appFileFullPath)) return appFileFullPath;
+
+            var teklaBinDir = Tekla.Structures.Dialog.StructuresInstallation.BinFolder;
+            if (!string.IsNullOrWhiteSpace(teklaBinDir))
+                appFileFullPath = Path.Combine(teklaBinDir, _appDirectory, _appName);
+
+            if (File.Exists(appFileFullPath)) return appFileFullPath;
+
+
+            var assemblyFile = System.Reflection.Assembly.GetExecutingAssembly().Location; // sometimes returns string empty
+            if (!string.IsNullOrWhiteSpace(assemblyFile))
+                appFileFullPath = Path.Combine(Path.GetDirectoryName(assemblyFile), _appName);
+
+            if (File.Exists(appFileFullPath)) return appFileFullPath;
+
+            throw new Exception($"Could not find {_appName} file");
+        }
+
+
+        private void CloseAllProcesses()
+        {
+            var processName = Path.GetFileNameWithoutExtension(_appName);
+            var processes = System.Diagnostics.Process.GetProcessesByName(processName);
+
+            if (processes != null)
+            {
+                foreach (var process in processes)
+                {
+                    process.Kill();
+                }
+            }
+        }
+    }
+}
